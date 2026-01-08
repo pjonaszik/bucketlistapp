b762be58bcaa1be5840c5aeb51c03156640fa120
pjonaszik <lionus1@live.fr>
2026-01-08 15:24:42 +0100

    bucket list

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,11 +7,13 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  BucketItem: a
     .model({
-      content: a.string(),
+      title: a.string(),
+      description: a.string(),
+      image: a.string(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [allow.owner()]),  // Restrict access to the owner
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +21,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
